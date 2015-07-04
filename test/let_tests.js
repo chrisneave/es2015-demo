6489f67e37a39828a2553c0913c4f1ee5b201835
Chris Neave <chrispneave@gmail.com>
2015-07-04 22:17:39 +0100

    Use string for all tests

--- test/let_tests.js
+++ test/let_tests.js
@@ -1,8 +1,8 @@
+'use strict';
 var expect = require('chai').expect;
 
 describe('let', function() {
   it('variables declared with let are accessible within nested blocks', function() {
-    'use strict';
     let foo = 'bar';
 
     if (true) {
