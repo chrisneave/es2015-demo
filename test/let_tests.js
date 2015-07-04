4902ee4561b33e747a6944f293364c7a3546464d
Chris Neave <chrispneave@gmail.com>
2015-07-04 22:23:23 +0100

    let variables not accessible outside block

--- test/let_tests.js
+++ test/let_tests.js
@@ -2,6 +2,23 @@
 var expect = require('chai').expect;
 
 describe('let', function() {
+  it('variables declared with let are not accessible in other blocks within the same function', function() {
+    var a = 1;
+
+    if (true) {
+      let foo = 'bar';
+    }
+
+    try {
+      a = foo;
+    }
+    catch(err) {
+      expect(err).to.be.an.instanceOf(ReferenceError);
+    }
+
+    expect(a).to.equal(1);
+  });
+
   it('variables declared with let are accessible within nested blocks', function() {
     let foo = 'bar';
 
